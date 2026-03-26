7bd659b51cde481764a0241ac8c18d87e8c2ea43
allenl <a9115077@gapps.ntust.edu.tw>
2026-03-26 14:22:27 +0800

    Trigger re-scan for build break verification

--- routes/index.js
+++ routes/index.js
@@ -386,3 +386,4 @@ router.get('/file', function(req, res) {
     var content = fs.readFileSync('/data/' + filename, 'utf8');
     res.send(content);
 });
+// PR flow build break test - Thu Mar 26 02:22:27 PM CST 2026
